de7e5ffb54ae749280192f44a0787f30c788e0f8
Sintry1 <Dominic_smith2012@hotmail.co.uk>
2024-08-20 13:35:23 +0200

    Created functionality to register sensor button press

--- src/ControllerDriver.cpp
+++ src/ControllerDriver.cpp
@@ -6,17 +6,17 @@
 #include <math.h>
 #include <Windows.h>
 
-ControllerDriver::ControllerDriver(uint8_t DeviceID, ControllerDriver::Handedness handedness) :
-	deviceID_(DeviceID),
-	handedness_(handedness)
+#define CHECK_BIT(var, pos) ((var) & (1 << (pos)))
+
+ControllerDriver::ControllerDriver(uint8_t DeviceID, ControllerDriver::Handedness handedness) : deviceID_(DeviceID), handedness_(handedness)
 {
 }
 
 EVRInitError ControllerDriver::Activate(uint32_t unObjectId)
 {
-	this->device_index_ = unObjectId; //unique ID for your driver
+	this->device_index_ = unObjectId; // unique ID for your driver
 
-	PropertyContainerHandle_t props = VRProperties()->TrackedDeviceToPropertyContainer(this->device_index_); //this gets a container object where you store all the information about your driver
+	PropertyContainerHandle_t props = vr::VRProperties()->TrackedDeviceToPropertyContainer(this->device_index_); // this gets a container object where you store all the information about your driver
 
 	VRDriverInput()->CreateBooleanComponent(props, "/input/a/click", &this->a_button_click_component_);
 	VRDriverInput()->CreateBooleanComponent(props, "/input/a/touch", &this->a_button_touch_component_);
@@ -37,50 +37,54 @@ EVRInitError ControllerDriver::Activate(uint32_t unObjectId)
 	VRProperties()->SetBoolProperty(props, Prop_HasControllerComponent_Bool, true);
 
 	// Give SteamVR a hint at what hand this controller is for
-	if (this->handedness_ == Handedness::LEFT) {
-		VRProperties()->SetInt32Property(props, Prop_ControllerRoleHint_Int32, ETrackedControllerRole::TrackedControllerRole_LeftHand); //tells OpenVR what kind of device this is
+	if (this->handedness_ == Handedness::LEFT)
+	{
+		VRProperties()->SetInt32Property(props, Prop_ControllerRoleHint_Int32, ETrackedControllerRole::TrackedControllerRole_LeftHand); // tells OpenVR what kind of device this is
 	}
-	else if (this->handedness_ == Handedness::RIGHT) {
-		VRProperties()->SetInt32Property(props, Prop_ControllerRoleHint_Int32, ETrackedControllerRole::TrackedControllerRole_RightHand); //tells OpenVR what kind of device this is
+	else if (this->handedness_ == Handedness::RIGHT)
+	{
+		VRProperties()->SetInt32Property(props, Prop_ControllerRoleHint_Int32, ETrackedControllerRole::TrackedControllerRole_RightHand); // tells OpenVR what kind of device this is
 	}
-	else {
-		VRProperties()->SetInt32Property(props, Prop_ControllerRoleHint_Int32, ETrackedControllerRole::TrackedControllerRole_OptOut); //tells OpenVR what kind of device this is
+	else
+	{
+		VRProperties()->SetInt32Property(props, Prop_ControllerRoleHint_Int32, ETrackedControllerRole::TrackedControllerRole_OptOut); // tells OpenVR what kind of device this is
 	}
 
-	VRProperties()->SetStringProperty(props, Prop_InputProfilePath_String, "{Amfitrack}/input/controller_profile.json"); //tell OpenVR where to get your driver's Input Profile
+	VRProperties()->SetStringProperty(props, Prop_InputProfilePath_String, "{Amfitrack}/input/controller_profile.json"); // tell OpenVR where to get your driver's Input Profile
 
 	std::stringstream ss;
-	ss << static_cast<int>(this->deviceID_);  // Convert to int for stream insertion
+	ss << static_cast<int>(this->deviceID_); // Convert to int for stream insertion
 	std::string message = "Initialized device: " + ss.str();
 	VRDriverLog()->Log(message.c_str());
 
-	//VRDriverInput()->CreateScalarComponent(props, "/input/joystick/y", &joystickYHandle, EVRScalarType::VRScalarType_Absolute,
-	//	EVRScalarUnits::VRScalarUnits_NormalizedTwoSided); //sets up handler you'll use to send joystick commands to OpenVR with, in the Y direction (forward/backward)
-	//VRDriverInput()->CreateScalarComponent(props, "/input/trackpad/y", &trackpadYHandle, EVRScalarType::VRScalarType_Absolute,
-	//	EVRScalarUnits::VRScalarUnits_NormalizedTwoSided); //sets up handler you'll use to send trackpad commands to OpenVR with, in the Y direction
-	//VRDriverInput()->CreateScalarComponent(props, "/input/joystick/x", &joystickXHandle, EVRScalarType::VRScalarType_Absolute,
-	//	EVRScalarUnits::VRScalarUnits_NormalizedTwoSided); //Why VRScalarType_Absolute? Take a look at the comments on EVRScalarType.
-	//VRDriverInput()->CreateScalarComponent(props, "/input/trackpad/x", &trackpadXHandle, EVRScalarType::VRScalarType_Absolute,
-	//	EVRScalarUnits::VRScalarUnits_NormalizedTwoSided); //Why VRScalarUnits_NormalizedTwoSided? Take a look at the comments on EVRScalarUnits.
-	
-	//The following properites are ones I tried out because I saw them in other samples, but I found they were not needed to get the sample working.
-	//There are many samples, take a look at the openvr_header.h file. You can try them out.
+	// Uncomment and implement these if needed
+	// VRDriverInput()->CreateScalarComponent(props, "/input/joystick/y", &joystickYHandle, EVRScalarType::VRScalarType_Absolute,
+	//     EVRScalarUnits::VRScalarUnits_NormalizedTwoSided); //sets up handler you'll use to send joystick commands to OpenVR with, in the Y direction (forward/backward)
+	// VRDriverInput()->CreateScalarComponent(props, "/input/trackpad/y", &trackpadYHandle, EVRScalarType::VRScalarType_Absolute,
+	//     EVRScalarUnits::VRScalarUnits_NormalizedTwoSided); //sets up handler you'll use to send trackpad commands to OpenVR with, in the Y direction
+	// VRDriverInput()->CreateScalarComponent(props, "/input/joystick/x", &joystickXHandle, EVRScalarType::VRScalarType_Absolute,
+	//     EVRScalarUnits::VRScalarUnits_NormalizedTwoSided); //Why VRScalarType_Absolute? Take a look at the comments on EVRScalarType.
+	// VRDriverInput()->CreateScalarComponent(props, "/input/trackpad/x", &trackpadXHandle, EVRScalarType::VRScalarType_Absolute,
+	//     EVRScalarUnits::VRScalarUnits_NormalizedTwoSided); //Why VRScalarUnits_NormalizedTwoSided? Take a look at the comments on EVRScalarUnits.
 
-	//VRProperties()->SetUint64Property(props, Prop_CurrentUniverseId_Uint64, 2);
-	//VRProperties()->SetBoolProperty(props, Prop_HasControllerComponent_Bool, true);
-	//VRProperties()->SetBoolProperty(props, Prop_NeverTracked_Bool, true);
-	//VRProperties()->SetInt32Property(props, Prop_Axis0Type_Int32, k_eControllerAxis_TrackPad);
-	//VRProperties()->SetInt32Property(props, Prop_Axis2Type_Int32, k_eControllerAxis_Joystick);
-	//VRProperties()->SetStringProperty(props, Prop_SerialNumber_String, "Amfitrack_controler_serial");
-	//VRProperties()->SetStringProperty(props, Prop_RenderModelName_String, "vr_controller_vive_1_5");
-	//uint64_t availableButtons = ButtonMaskFromId(k_EButton_SteamVR_Touchpad) |
-	//	ButtonMaskFromId(k_EButton_IndexController_JoyStick);
-	//VRProperties()->SetUint64Property(props, Prop_SupportedButtons_Uint64, availableButtons);
+	// The following properties are ones I tried out because I saw them in other samples, but I found they were not needed to get the sample working.
+	// There are many samples, take a look at the openvr_header.h file. You can try them out.
+
+	// VRProperties()->SetUint64Property(props, Prop_CurrentUniverseId_Uint64, 2);
+	// VRProperties()->SetBoolProperty(props, Prop_HasControllerComponent_Bool, true);
+	// VRProperties()->SetBoolProperty(props, Prop_NeverTracked_Bool, true);
+	// VRProperties()->SetInt32Property(props, Prop_Axis0Type_Int32, k_eControllerAxis_TrackPad);
+	// VRProperties()->SetInt32Property(props, Prop_Axis2Type_Int32, k_eControllerAxis_Joystick);
+	// VRProperties()->SetStringProperty(props, Prop_SerialNumber_String, "Amfitrack_controler_serial");
+	// VRProperties()->SetStringProperty(props, Prop_RenderModelName_String, "vr_controller_vive_1_5");
+	// uint64_t availableButtons = ButtonMaskFromId(k_EButton_SteamVR_Touchpad) |
+	//     ButtonMaskFromId(k_EButton_IndexController_JoyStick);
+	// VRProperties()->SetUint64Property(props, Prop_SupportedButtons_Uint64, availableButtons);
 
 	return VRInitError_None;
 }
 
-void normalizeQuaternion(HmdQuaternion_t& q)
+void normalizeQuaternion(HmdQuaternion_t &q)
 {
 	double norm = std::sqrt(q.w * q.w + q.x * q.x + q.y * q.y + q.z * q.z);
 	q.w /= norm;
@@ -92,22 +96,22 @@ void normalizeQuaternion(HmdQuaternion_t& q)
 HmdQuaternion_t rotate(HmdQuaternion_t a, HmdQuaternion_t b)
 {
 	HmdQuaternion_t result;
-	result.w = a.w * b.w - a.x * b.x - a.y * b.y - a.z * b.z; 
-	result.x = a.w * b.x + a.x * b.w + a.y * b.z - a.z * b.y; 
-	result.y = a.w * b.y - a.x * b.z + a.y * b.w + a.z * b.x; 
-	result.z = a.w * b.z + a.x * b.y - a.y * b.x + a.z * b.w; 
+	result.w = a.w * b.w - a.x * b.x - a.y * b.y - a.z * b.z;
+	result.x = a.w * b.x + a.x * b.w + a.y * b.z - a.z * b.y;
+	result.y = a.w * b.y - a.x * b.z + a.y * b.w + a.z * b.x;
+	result.z = a.w * b.z + a.x * b.y - a.y * b.x + a.z * b.w;
 
 	return result;
 }
 
 DriverPose_t ControllerDriver::GetPose()
 {
-	AMFITRACK& AMFITRACK = AMFITRACK::getInstance();
-	DriverPose_t pose = { 0 }; //This Amfitrack doesn't use Pose, so this method is just returning a default Pose.
+	AMFITRACK &AMFITRACK = AMFITRACK::getInstance();
+	DriverPose_t pose = {0}; // This Amfitrack doesn't use Pose, so this method is just returning a default Pose.
 	if (!AMFITRACK.getDeviceActive(this->deviceID_))
 	{
 		std::stringstream ss;
-		ss << static_cast<int>(this->deviceID_);  // Convert to int for stream insertion
+		ss << static_cast<int>(this->deviceID_); // Convert to int for stream insertion
 		std::string message = "No Pose for this device: " + ss.str();
 		VRDriverLog()->Log(message.c_str());
 		pose.poseIsValid = false;
@@ -117,15 +121,13 @@ DriverPose_t ControllerDriver::GetPose()
 	lib_AmfiProt_Amfitrack_Pose_t position;
 	AMFITRACK.getDevicePose(this->deviceID_, &position);
 
-
-
 	pose.deviceIsConnected = true;
 	pose.poseIsValid = true;
 	pose.result = TrackingResult_Running_OK;
 	pose.willDriftInYaw = false;
 	pose.shouldApplyHeadModel = false;
 	pose.qDriverFromHeadRotation.w = pose.qWorldFromDriverRotation.w = pose.qRotation.w = 1.0;
-	
+
 	pose.vecPosition[0] = -position.position_x_in_m;
 	pose.vecPosition[1] = position.position_y_in_m;
 	pose.vecPosition[2] = -position.position_z_in_m;
@@ -157,8 +159,6 @@ DriverPose_t ControllerDriver::GetPose()
 
 	normalizeQuaternion(pose.qRotation);
 
-
-
 	x << static_cast<double>(pose.qRotation.x);
 	y << static_cast<double>(pose.qRotation.y);
 	z << static_cast<double>(pose.qRotation.z);
@@ -170,34 +170,40 @@ DriverPose_t ControllerDriver::GetPose()
 
 void ControllerDriver::RunFrame()
 {
+
 	VRServerDriverHost()->TrackedDevicePoseUpdated(this->device_index_, GetPose(), sizeof(vr::DriverPose_t));
 
-	if (GetAsyncKeyState(0x41 /* E */) != 0) {
-		VRDriverInput()->UpdateBooleanComponent(this->a_button_click_component_, true, 0);
-		VRDriverInput()->UpdateBooleanComponent(this->a_button_touch_component_, true, 0);
+	AMFITRACK &AMFITRACK = AMFITRACK::getInstance();
+	lib_AmfiProt_Amfitrack_Sensor_Measurement_t sensorMeasurement;
+	AMFITRACK.getSensorMeasurements(this->deviceID_, &sensorMeasurement);
+	uint16_t gpio_state = sensorMeasurement.gpio_state;
+
+	bool ButtonPressed = CHECK_BIT(gpio_state, 0);
+
+	if (this->handedness_ == Handedness::RIGHT)
+	{
+		if (ButtonPressed)
+		{
+			VRDriverLog()->Log("Right sensor button pressed");
+		}
+		VRDriverInput()->UpdateBooleanComponent(this->a_button_click_component_, ButtonPressed, 0);
+		VRDriverInput()->UpdateBooleanComponent(this->a_button_touch_component_, ButtonPressed, 0);
 	}
-	else {
-		VRDriverInput()->UpdateBooleanComponent(this->a_button_click_component_, false, 0);
-		VRDriverInput()->UpdateBooleanComponent(this->a_button_touch_component_, false, 0);
+	else if (this->handedness_ == Handedness::LEFT)
+	{
+		if (ButtonPressed)
+		{
+			VRDriverLog()->Log("Left sensor button pressed");
+		}
+		VRDriverInput()->UpdateBooleanComponent(this->b_button_click_component_, ButtonPressed, 0);
+		VRDriverInput()->UpdateBooleanComponent(this->b_button_touch_component_, ButtonPressed, 0);
 	}
 
-	if (GetAsyncKeyState(0x51 /* Q */) != 0) {
-		VRDriverInput()->UpdateBooleanComponent(this->b_button_click_component_, true, 0);
-		VRDriverInput()->UpdateBooleanComponent(this->b_button_touch_component_, true, 0);
-	}
-	else {
-		VRDriverInput()->UpdateBooleanComponent(this->b_button_click_component_, false, 0);
-		VRDriverInput()->UpdateBooleanComponent(this->b_button_touch_component_, false, 0);
-	}
-
-	if (GetAsyncKeyState(0x44 /* D */) != 0) {
-		VRDriverInput()->UpdateBooleanComponent(this->trigger_click_component_, true, 0);
-		VRDriverInput()->UpdateBooleanComponent(this->trigger_touch_component_, true, 0);
-	}
-	else {
-		VRDriverInput()->UpdateBooleanComponent(this->trigger_click_component_, false, 0);
-		VRDriverInput()->UpdateBooleanComponent(this->trigger_touch_component_, false, 0);
-	}
+	// if (GetAsyncKeyState(0x44 /* D acting as Trigger */) != 0)
+	// {
+	// 	VRDriverInput()->UpdateBooleanComponent(this->trigger_click_component_, 1, 0);
+	// 	VRDriverInput()->UpdateBooleanComponent(this->trigger_touch_component_, 1, 0);
+	// }
 }
 
 void ControllerDriver::Deactivate()
@@ -205,12 +211,12 @@ void ControllerDriver::Deactivate()
 	this->device_index_ = k_unTrackedDeviceIndexInvalid;
 }
 
-void* ControllerDriver::GetComponent(const char* pchComponentNameAndVersion)
+void *ControllerDriver::GetComponent(const char *pchComponentNameAndVersion)
 {
-	//I found that if this method just returns null always, it works fine. But I'm leaving the if statement in since it doesn't hurt.
-	//Check out the IVRDriverInput_Version declaration in openvr_driver.h. You can search that file for other _Version declarations 
-	//to see other components that are available. You could also put a log in this class and output the value passed into this 
-	//method to see what OpenVR is looking for.
+	// I found that if this method just returns null always, it works fine. But I'm leaving the if statement in since it doesn't hurt.
+	// Check out the IVRDriverInput_Version declaration in openvr_driver.h. You can search that file for other _Version declarations
+	// to see other components that are available. You could also put a log in this class and output the value passed into this
+	// method to see what OpenVR is looking for.
 	if (strcmp(IVRDriverInput_Version, pchComponentNameAndVersion) == 0)
 	{
 		return this;
@@ -220,7 +226,7 @@ void* ControllerDriver::GetComponent(const char* pchComponentNameAndVersion)
 
 void ControllerDriver::EnterStandby() {}
 
-void ControllerDriver::DebugRequest(const char* pchRequest, char* pchResponseBuffer, uint32_t unResponseBufferSize) 
+void ControllerDriver::DebugRequest(const char *pchRequest, char *pchResponseBuffer, uint32_t unResponseBufferSize)
 {
 	if (unResponseBufferSize >= 1)
 	{
--- src/DeviceProvider.cpp
+++ src/DeviceProvider.cpp
@@ -1,59 +1,74 @@
 #include <DeviceProvider.h>
 #include "amfitrack_cpp_SDK/Amfitrack.hpp"
 
+
+
+// Initializes the device provider
 EVRInitError DeviceProvider::Init(IVRDriverContext* pDriverContext)
 {
+    // Initialize the server driver context
     EVRInitError initError = InitServerDriverContext(pDriverContext);
     if (initError != EVRInitError::VRInitError_None)
     {
-        return initError;
+        return initError; // Return error if initialization fails
     }
     
-    VRDriverLog()->Log("Initializing Amfitrack controller"); //this is how you log out Steam's log file.
+    VRDriverLog()->Log("Initializing Amfitrack controller"); // Log initialization message
 
-    /* Create instance of amfitrack */
+    // Create instance of Amfitrack
     AMFITRACK& AMFITRACK = AMFITRACK::getInstance();
-    /* Initializes the USB and connects to the devices */
+    // Initialize the USB and connect to the devices
     AMFITRACK.initialize_amfitrack();
-    /* Starts the main thread, that read the data */
+    // Start the main thread that reads the data
     AMFITRACK.start_amfitrack_task();
 
-    VRDriverLog()->Log("Amfitrack started"); //this is how you log out Steam's log file.
+    VRDriverLog()->Log("Amfitrack started"); // Log Amfitrack started message
 
+    // Initialize left controller driver
     controllerDriverLeft = new ControllerDriver(2, ControllerDriver::Handedness::LEFT);
-    VRServerDriverHost()->TrackedDeviceAdded("Amfitrack_controller_left", TrackedDeviceClass_Controller, controllerDriverLeft); //add all your devices like this.
+    VRServerDriverHost()->TrackedDeviceAdded("Amfitrack_controller_left", TrackedDeviceClass_Controller, controllerDriverLeft); // Add left controller device
 
+    // Initialize right controller driver
     controllerDriverRight = new ControllerDriver(3, ControllerDriver::Handedness::RIGHT);
-    VRServerDriverHost()->TrackedDeviceAdded("Amfitrack_controller_right", TrackedDeviceClass_Controller, controllerDriverRight); //add all your devices like this.
+    VRServerDriverHost()->TrackedDeviceAdded("Amfitrack_controller_right", TrackedDeviceClass_Controller, controllerDriverRight); // Add right controller device
 
-    VRDriverLog()->Log("Initializing completed!"); //this is how you log out Steam's log file.
+    VRDriverLog()->Log("Initializing completed!"); // Log initialization completed message
 
-    return vr::VRInitError_None;
+    return vr::VRInitError_None; // Return no error
 }
 
+// Cleans up the device provider
 void DeviceProvider::Cleanup()
 {
+    // Delete left and right controller drivers
     delete controllerDriverLeft;
     delete controllerDriverRight;
-    controllerDriverLeft = NULL;
-    controllerDriverRight = NULL;
-}
-const char* const* DeviceProvider::GetInterfaceVersions()
-{
-    return k_InterfaceVersions;
+    controllerDriverLeft = NULL; // Set left controller driver to NULL
+    controllerDriverRight = NULL; // Set right controller driver to NULL
 }
 
+// Returns the interface versions
+const char* const* DeviceProvider::GetInterfaceVersions()
+{
+    return k_InterfaceVersions; // Return interface versions
+}
+
+// Runs a frame for the device provider
 void DeviceProvider::RunFrame()
 {
+    // Run frame for left and right controller drivers
     controllerDriverLeft->RunFrame();
     controllerDriverRight->RunFrame();
 }
 
+// Indicates whether to block standby mode
 bool DeviceProvider::ShouldBlockStandbyMode()
 {
-    return false;
+    return false; // Do not block standby mode
 }
 
+// Enters standby mode
 void DeviceProvider::EnterStandby() {}
 
+// Leaves standby mode
 void DeviceProvider::LeaveStandby() {}
