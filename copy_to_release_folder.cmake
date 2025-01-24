64e608ebf2a874b9c01ee9a76776f00a677bdcda
Kenn Fischer <kf@amfitech.dk>
2025-01-24 08:05:25 +0100

    Added support for making release folder with the complete driver. Changed so all files were copied at once.

--- copy_to_release_folder.cmake
+++ copy_to_release_folder.cmake
@@ -1,6 +1,6 @@
 # Get the source and destination directories from the command line arguments
 set(SOURCE_DIR "${CMAKE_CURRENT_LIST_DIR}/driver/amfitrack/")
-set(DEST_DIR "C:/Program Files (x86)/Steam/steamapps/common/SteamVR/drivers/Amfitrack_SteamVR/")
+set(DEST_DIR "${CMAKE_CURRENT_LIST_DIR}/release/Amfitrack_SteamVR/")
 
 # Print debug messages
 message(STATUS "Copying files from ${SOURCE_DIR} to ${DEST_DIR}")
